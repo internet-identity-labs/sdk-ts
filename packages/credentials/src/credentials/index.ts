3ef066f1d3e51af3d0496593b13dfab0c582c363
Jorgen <jorgen@buildnode.io>
2022-06-23 15:14:08 -0400

    feat: updated provider url

--- packages/credentials/src/credentials/index.ts
+++ packages/credentials/src/credentials/index.ts
@@ -1,7 +1,9 @@
 export * from './client';
 export * from './provider';
 
-export const defaultProvider = new URL(`https://nfid.one/credential`);
+export const defaultProvider = new URL(
+    `https://nfid.one/credential/verified-phone-number`
+);
 
 export function validateEventOrigin(
     event: MessageEvent,
