914968ead19594d38d0c8e0d4310a90d4fb1dc8b
Pavlo Chorniy <>
2023-02-02 08:46:46 +0100

    fix([sc-6090]): removed iframe-auth types.ts

--- packages/iframe-auth/src/types.ts
+++
@@ -1,9 +0,0 @@
-import { Identity } from '@dfinity/agent';
-
-export type IFrameAuthClientResult = Identity;
-export type IFrameAuthClientEvents =
-  | { kind: 'Ready' }
-  | {
-      kind: 'Authenticated';
-      result: IFrameAuthClientResult;
-    };
