ea01983109f8e0d1cd30d7bd95d661fba093e3b7
Dmitrii <dmitrii@identityLabs.ooo>
2023-05-03 16:49:50 +0400

    feat([sc-6614]): support switching networks [sc-6614]

--- packages/embed/src/lib/inpage-provider.spec.ts
+++ packages/embed/src/lib/inpage-provider.spec.ts
@@ -1,8 +1,9 @@
+import { ethers } from 'ethers';
 import { NFIDInpageProvider } from './inpage-provider';
 
 describe('inpage-provider', () => {
-  const nfidInpageProvider = new NFIDInpageProvider();
+  const nfidInpageProvider = new NFIDInpageProvider(5, undefined as any as ethers.providers.JsonRpcProvider);
   it('should return chainId', () => {
-    expect(nfidInpageProvider.chainId).toEqual('0x5');
+    expect(nfidInpageProvider.chainId).toEqual('0x05');
   });
 });
