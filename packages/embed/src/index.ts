c376121804c6cb8f37424b6033c76672aa66efd1
Philipp Litzenberger <philipp@identitylabs.ooo>
2023-12-11 14:05:24 +0100

    chore: add testing logger

--- packages/embed/src/index.ts
+++ packages/embed/src/index.ts
@@ -2,3 +2,5 @@ export * from './lib/iframe/manager';
 export * from './lib/nfid_eth';
 export * from './lib/nfid';
 export * from './lib/authentication';
+
+console.debug('@nfid/embed', { version: '0.10.0-alpha.12' });
